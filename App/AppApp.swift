9ccbb13dfc17bc94f8b755bd52fab64520017fb1
Jacob Lee <jacob.lee.jiege@sji.edu.sg>
2024-07-06 15:47:11 +0800

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by J Family on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
